88ebcf2484a5d6419a7d3db0828f0d30713988b0
Einar Sigurðsson <einarsig16@ru.is>
2018-10-10 18:53:22 +0000

    Added support for single number in input

--- add.js
+++ add.js
@@ -1,6 +1,14 @@
 function Add (numbers) {
-    if (numbers === '')
+    // Check if string is empty
+    if (numbers === '') {
         return 0
+    }
+
+    // Check if string contains one number
+    console.log(numbers.indexOf(','))
+    if(numbers.indexOf(',') < 0) {
+        return parseInt(numbers)
+    }
 }
 
 module.exports = Add
