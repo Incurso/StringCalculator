4113ac1782906af47f26767913929eb2e15bd7d8
Einar Sigurðsson <einarsig16@ru.is>
2018-10-10 18:59:14 +0000

    Added test for two numbers

--- add.test.js
+++ add.test.js
@@ -4,6 +4,10 @@ it('Should return zero on an empty string', () => {
     expect(add('')).toBe(0)
 })
 
-it('Should return number', () => {
+it('Should return seven', () => {
     expect(add('7')).toBe(7)
+})
+
+it('Should return five', () => {
+    expect(add('2,3')).toBe(5)
 })
