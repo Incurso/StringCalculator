931a5901369804354617e6692ac5a731c677e72d
Einar Sigurðsson <einarsig16@ru.is>
2018-10-10 18:52:34 +0000

    Added test for single number

--- add.test.js
+++ add.test.js
@@ -3,3 +3,7 @@ const add = require('./add')
 it('Should return zero on an empty string', () => {
     expect(add('')).toBe(0)
 })
+
+it('Should return number', () => {
+    expect(add('7')).toBe(7)
+})
