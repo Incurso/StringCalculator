756589e822669922bd0faa8a20a428288903eb5c
Einar Sigurðsson <einarsig16@ru.is>
2018-10-11 18:31:51 +0000

    Refactored code so that the numbers are parsed when they are split

--- add.js
+++ add.js
@@ -1,19 +1,20 @@
 function Add (numbers) {
-    // Check if string is empty
-    if (numbers === '') {
-        return 0
-    }
+  // Check if string is empty
+  if (numbers === '') {
+    return 0
+  }
 
-    // Check if string contains one number
-    if (numbers.indexOf(',') < 0) {
-        return parseInt(numbers)
-    } else {
-        nums = numbers.split(',')
-        
-        if (nums.length === 2) {
-            return parseInt(nums[0]) + parseInt(nums[1])
-        }
+  // Check if string contains one number
+  if (numbers.indexOf(',') < 0) {
+    return parseInt(numbers)
+  } else {
+    // Split the string on commas and parse the value to int
+    let nums = numbers.split(',').map(n => parseInt(n))
+
+    if (nums.length === 2) {
+      return nums[0] + nums[1]
     }
+  }
 }
 
 module.exports = Add
