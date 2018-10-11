23699007eaed6eb8730225465b271df190fab886
Einar Sigurðsson <einarsig16@ru.is>
2018-10-11 19:06:39 +0000

    Added regex to handle split on newline and comma

--- add.js
+++ add.js
@@ -5,22 +5,13 @@ function Add (numbers) {
   }
 
   // Check if string contains one number
-  if (numbers.indexOf(',') < 0) {
+  if (numbers.search(/[,\n]/) < 0) {
     return parseInt(numbers)
   } else {
     // Split the string on commas and parse the value to int
-    let nums = numbers.split(',').map(n => parseInt(n))
+    let nums = numbers.split(/[,\n]/).map(n => parseInt(n))
 
     if (nums.length > 1) {
-      /*
-      let total = 0
-
-      for (let i = 0; i < nums.length; i++) {
-        total += nums[i]
-      }
-
-      return total
-      */
       return nums.reduce((a, b) => a + b)
     }
   }
