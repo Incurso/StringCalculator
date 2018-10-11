e0c4e5e74f275b53b961eb4418eac509309ccac7
Einar Sigurðsson <einarsig16@ru.is>
2018-10-11 18:30:56 +0000

    Changed from 4 spaces to two spaced to comply with standardjs

--- add.test.js
+++ add.test.js
@@ -1,13 +1,13 @@
 const add = require('./add')
 
 it('Should return zero on an empty string', () => {
-    expect(add('')).toBe(0)
+  expect(add('')).toBe(0)
 })
 
 it('Should return seven', () => {
-    expect(add('7')).toBe(7)
+  expect(add('7')).toBe(7)
 })
 
 it('Should return five', () => {
-    expect(add('2,3')).toBe(5)
+  expect(add('2,3')).toBe(5)
 })
