1c3bf931485f34bcceea001814d1a10ddad2f482
Einar Sigurðsson <einarsig16@ru.is>
2018-10-12 00:15:12 +0000

    Changed funciton to throw error if there is a negative number in the string

--- add.js
+++ add.js
@@ -6,10 +6,24 @@ function Add (numbers) {
 
   // Check if string contains one number
   if (numbers.search(/[,\n]/) < 0) {
-    return parseInt(numbers)
+    let num = parseInt(numbers)
+
+    if (num < 0) {
+      throw new Error(`Negatives not allowed: ${num}`)
+    }
+
+    return num
   } else {
     // Split the string on commas and parse the value to int
-    let nums = numbers.split(/[,\n]/).map(n => parseInt(n))
+    let nums = numbers.split(/[,\n]/).map(n => {
+      let num = parseInt(n)
+
+      if (num < 0) {
+        throw new Error(`Negatives not allowed: ${num}`)
+      }
+
+      return num
+    })
 
     if (nums.length > 1) {
       return nums.reduce((a, b) => a + b)
