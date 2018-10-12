3cda024b21ffdc09af8ad5ca4209cf5c9c0042c5
Einar Sigurðsson <einarsig16@ru.is>
2018-10-12 00:26:45 +0000

    Refactored the code

--- add.js
+++ add.js
@@ -4,31 +4,19 @@ function Add (numbers) {
     return 0
   }
 
-  // Check if string contains one number
-  if (numbers.search(/[,\n]/) < 0) {
-    let num = parseInt(numbers)
+  // Split the string on commas and parse the values to int
+  let nums = numbers.split(/[,\n]/).map(n => {
+    let num = parseInt(n)
 
     if (num < 0) {
       throw new Error(`Negatives not allowed: ${num}`)
     }
 
     return num
-  } else {
-    // Split the string on commas and parse the value to int
-    let nums = numbers.split(/[,\n]/).map(n => {
-      let num = parseInt(n)
+  })
 
-      if (num < 0) {
-        throw new Error(`Negatives not allowed: ${num}`)
-      }
-
-      return num
-    })
-
-    if (nums.length > 1) {
-      return nums.reduce((a, b) => a + b)
-    }
-  }
+  // Check if string contains one number
+  return nums.reduce((a, b) => a + b)
 }
 
 module.exports = Add
